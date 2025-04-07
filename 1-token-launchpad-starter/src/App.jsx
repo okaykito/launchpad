300a089c2fff8fdb20351f4ca9ea301eb4268b8b
okaykito <ksupadhyaya27@gmail.com>
2025-04-07 18:42:02 +0000

    wallet

--- 1-token-launchpad-starter/src/App.jsx
+++ 1-token-launchpad-starter/src/App.jsx
@@ -1,10 +1,25 @@
+import { WalletConnectButton, WalletDisconnectButton, WalletModalProvider, WalletMultiButton } from '@solana/wallet-adapter-react-ui'
 import './App.css'
 import { TokenLaunchpad } from './components/TokenLaunchpad'
+import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react'
+
 
 function App() {
   return (
+    <ConnectionProvider endpoint={"https://api.devnet.solana.com"}>
+    <WalletProvider wallets={[]} autoConnect>
+    <WalletModalProvider>
+    <WalletMultiButton></WalletMultiButton>
+    <WalletDisconnectButton></WalletDisconnectButton>
+    <div>
+      Heya
+    </div>
     <TokenLaunchpad></TokenLaunchpad>
+    </WalletModalProvider>
+    </WalletProvider>
+    </ConnectionProvider>
   )
 }
 
 export default App
+
